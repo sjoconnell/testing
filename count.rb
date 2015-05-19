35e017142db0b65dc4de1a9b50b80874d168f36d
Sam O'Connell <sjo773@gmail.com>
2015-05-18 20:38:58 -0500

    initial commit

---
+++ count.rb
@@ -0,0 +1,10 @@
+count = 0
+
+counts = []
+
+11.times do
+  counts << count
+  count += 1
+end
+
+puts counts
---
+++ foods.rb
@@ -0,0 +1,9 @@
+foods = []
+
+5.times do 
+puts "What's your favorite food?"
+food = gets.chomp
+foods << food
+end
+
+p foods
